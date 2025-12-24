66093641a9d3554f691e9a5e1ae7cff880ef8aa3
Swastik Mandal <swastik.codes.2010@gmail.com>
2025-12-24 17:44:03 +0530

    Add strings type practice

---
+++ 02_basics/strings.py
@@ -0,0 +1,12 @@
+text =" Hello world "
+print(text.upper())#output: HELLO WORLD
+print(text.lower())#output: hello world
+print(text.title())#output: Hello World
+print(text.capitalize())#output: Hello world
+print(text.swapcase())#output: hELLO WORLD
+print(text.find('world'))#output: 6
+print(text.count('o'))#output:7
+print(text.strip())#output:Helloworld
+print(text.replace('o','*'))#output: Hell* w*rld
+print(len(text))#output: 13
+
