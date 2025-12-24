7ee50ea398c5c9bc86917ea3722864914197f52e
Swastik Mandal <swastik.codes.2010@gmail.com>
2025-12-24 17:30:24 +0530

    Add comments in variables practice

--- 02_basics/variables.py
+++ 02_basics/variables.py
@@ -1,4 +1,4 @@
-character_name = "Swastik"
-character_age = 15
+character_name = "Swastik"  # character_name variable holds a string
+character_age = 15 # character_age variable holds a number
 print("There was a boy named"+character_name+",")
 print("at "+str(character_age)+" he wanted to learn python")
