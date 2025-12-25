ebd064687f72cefaba205cd0bfd1ff1b07d4ca3d
Swastik Mandal <swastik.codes.2010@gmail.com>
2025-12-25 18:48:10 +0530

    add: number operations and math functions

---
+++ 02_basics/numbers.py
@@ -0,0 +1,29 @@
+#Basic operations
+print(5 + 3)
+print(10 - 4)
+print(6 * 7)
+print(20 / 4)
+print(10 % 3)
+
+#Order of operations
+print(3 + 4 * 2)      # multiplication first
+print((3 + 4) * 2)    # brackets first
+
+#Numbers + strings
+age = 15
+print("My age is " + str(age))
+
+#Useful math functions
+print(abs(-5))
+print(pow(2, 3))
+print(max(4, 9, 1))
+print(min(4, 9, 1))
+round_num = round(3.6)
+print(round_num)
+
+#Import math module
+import math
+
+print(math.floor(3.7))
+print(math.ceil(3.2))
+print(math.sqrt(49))
