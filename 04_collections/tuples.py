3ee3715025431fcfd57df08592d123ec87056513
Swastik Mandal <swastik.codes.2010@gmail.com>
2025-12-30 18:31:37 +0530

    add: tuples and immutability concept

---
+++ 04_collections/tuples.py
@@ -0,0 +1,11 @@
+coordinates = (4, 5)
+
+print(coordinates)
+print(coordinates[0])
+print(coordinates[1])
+
+# Tuples are immutable (cannot change values)
+# coordinates[0] = 10  # This would cause an error
+
+points = [(1, 2), (3, 4), (5, 6)]
+print(points)
